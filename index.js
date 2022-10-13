9cef8481f801cbd60f8c0767b72523b443ae3748
kkkovalchukkk <student.kovalchuk@yandex.ru>
2022-10-13 13:42:29 +0700

    finished header and main

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import './styles/index.scss';
 import 'resetcss';
+import 'boxicons';
+
+
+import Swiper, {
+    Navigation,
+} from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 7,
+    direction: 'horizontal',
+    modules: [Navigation],
+    navigation: {
+        nextEl: '.swp-next',
+        prevEl: '.swp-prev',
+    },
+});
